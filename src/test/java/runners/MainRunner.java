57141badba078863016ee33fe0c5fe569b51c227
AbenezerM <abenezermulugeta41@gmail.com>
2024-08-10 21:22:22 -0400

    Test runner reports

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -10,7 +10,13 @@ import io.cucumber.testng.CucumberOptions;
  * @param monochrome control formatting of the console output
  * @param dryRun if set to true cucumber checks if a step in scenario has
  * a corresponding step definition, but not actually run the step definitions
+ * @param plugin adds more information about our scenario steps in the console output and export report to html & json
  */
-@CucumberOptions(features = {"classpath:features"}, glue = {"step_definitions"}, tags = "@contact-us", monochrome = true, dryRun = false)
+@CucumberOptions(features = {"classpath:features"},
+        glue = {"step_definitions"},
+        tags = "@contact-us",
+        monochrome = true,
+        dryRun = false,
+        plugin = {"pretty", "html:target/cucumber-report", "json:target/cucumber-report.json"})
 public class MainRunner extends AbstractTestNGCucumberTests {
 }
